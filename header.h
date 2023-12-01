71d18080825fe6fe9741cdfa26349bd1123daa3e
Deepikasgowda29 <152603610+Deepikasgowda29@users.noreply.github.com>
2023-12-01 15:34:41 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define PORT 9000
+#define MAX 2000
+
+struct client{
+  int index;
+  int socketID;
+  struct sockaddr_in client_addr;
+  int len;
+  char status[20];
+};
+
+
+void * ChattingProcess(void * ClientDetail);
+
+void message(void* ClientDetail,char* clientMsg);
+
+void * doReceving(void* socketID);
