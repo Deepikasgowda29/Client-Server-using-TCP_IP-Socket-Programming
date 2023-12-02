a80e392e76b5630f1d208acc678c6cca909cac5b
Sunilp-patil <152605951+Sunilp-patil@users.noreply.github.com>
2023-12-02 13:10:58 +0530

    Command.c

--- Command.c
+++ Command.c
@@ -16,12 +16,12 @@ void *Command(void *ClientDetail) {
   FILE *fp;
   fp = fopen("history.txt", "a");
   if (fp == NULL) {
-    printf("\nMy file did not open\n");
+    perror("\nMy file did not open\n");
   }
 
   // Getting client index and socket id
-  int index = clientDetail->index;
-  int clientSocket = clientDetail->socketID;
+  uint8_t index = clientDetail->index;
+  uint8_t clientSocket = clientDetail->socketID;
 
   printf("[++] Client %d connected... \n", index + 1);
 
@@ -37,13 +37,13 @@ void *Command(void *ClientDetail) {
   memset(output, '\0', sizeof(output));
   strcpy(output, basicCommand);
   if (send(clientSocket, output, strlen(output), 0) < 0) {
-    printf(" [--] Can't send message... \n");
+    perror(" [--] Can't send message... \n");
     return NULL;
   }
   // Message processing
   while (1) {
     char clientMsg[MAX];
-    int CHAT = 0;
+    uint8_t CHAT = 0;
 
     // clearing the buffers
 
@@ -52,18 +52,15 @@ void *Command(void *ClientDetail) {
 
     // receving client message
     if (recv(clientSocket, clientMsg, sizeof(clientMsg), 0) < 0) {
-      printf(" [--] Couldn't receive client message... \n");
+      perror(" [--] Couldn't receive client message... \n");
       break;
     }
     fprintf(fp, "\n+++ CLIENT %d | client message : %s | client socket : %d\n",
             index + 1, clientMsg, clientSocket);
     printf("\n+++ CLIENT %d | client message : %s | client socket : %d\n",
            index + 1, clientMsg, clientSocket);
-    // fclose(fp);
 
-    // fputs(buffer,fp);
-
-    // if the message contains ':' delemeter then set CHAT = true;
+    // if the message contains ':' delemeter then set CHAT = true(1);
     char *ck;
     ck = strchr(clientMsg, ':');
     if (ck != NULL) {
@@ -75,22 +72,24 @@ void *Command(void *ClientDetail) {
       break;
     } else if (strcmp(clientMsg, "HELP") == 0) {
       strcpy(output, basicCommand);
-    } else if (strcmp(clientMsg, "LIST") == 0) {
-      int len = 0;
+    }
+
+    else if (strcmp(clientMsg, "LIST") == 0) {
+      int8_t len = 0;
       if (clientCount == 1) {
         len += sprintf(output + len,
                        "[res] Currently no other clients are connected...\n");
       } else {
         for (int i = 0; i < clientCount; i++) {
           if (i != index) {
-            len += sprintf(
-                output + len, "[res] Client: %d | soket: %d | status : %s\n",
-                (i + 1), Client[i].socketID,
-                Client[i].status); // sprintf returns the no of bytes that are
-                                   // written in array which is held by variable
-                                   // l so to append multiple sentance we need
-                                   // to increment output to l byte for getting
-                                   // that location
+            len += sprintf(output + len,
+                           "[res] Client: %d | soket: %d | status : %s\n",
+                           (i + 1), Client[i].socketID, Client[i].status);
+            // sprintf returns the no of bytes that are
+            // written in array which is held by variable
+            // len so to append multiple sentance we need
+            // to increment output to len byte for getting
+            // that location
           }
         }
       }
@@ -101,19 +100,19 @@ void *Command(void *ClientDetail) {
 
     else {
       // wrong command response
-      strcpy(output, "\n[res] WRONG INPUT COMMAND... PLEASE TRY AGAIN WITH "
-                     "VALID COMMAND...\n");
+      strcpy(output,
+             "\n[res] WRONG INPUT COMMAND... PLEASE ENTER VALID COMMAND...\n");
     }
 
-    // sending servers response to this client
+    // sending server response to this client
     if (send(clientSocket, output, strlen(output), 0) < 0) {
-      printf("\n[--] Can't send message... \n");
+      perror("\n[--] Can't send message... \n");
       break;
     }
   }
 
   // making this client status DE-ACTIVE and closing its socket
-  stpcpy(clientDetail->status, "DE_ACTIVE");
+  strcpy(clientDetail->status, "DE_ACTIVE");
   close(clientSocket);
   printf("\n[...] Client %d Exited...Thread id = %u \n\n", index + 1,
          (unsigned int)pthread_self());
