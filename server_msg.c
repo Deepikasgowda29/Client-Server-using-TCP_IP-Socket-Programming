ad728426576388ad741867be195d41f3548b1471
Sairaj0603 <152605079+Sairaj0603@users.noreply.github.com>
2023-12-02 08:37:20 -0800

    Update server_msg.c

--- server_msg.c
+++ server_msg.c
@@ -1,70 +1,75 @@
 
 #include "header.h"
 
-// Creatinng clint count global variable to check no of clients
-extern int clientCount;
+extern uint8_t clientCount;
 
 char output[MAX];
-// Declare and initializing pthread_t type variable and struct client variable for use
 extern struct client Client[10];
-extern pthread_t thread[10];
-
 
 void message(void* ClientDetail,char* clientMsg)
 {
-struct client* clientDetail = (struct client*) ClientDetail;
-int index = clientDetail -> index;
-  int clientSocket = clientDetail -> socketID;
+	struct client* clientDetail = (struct client*) ClientDetail;
+	uint8_t index = clientDetail -> index;
+  	uint8_t clientSocket = clientDetail -> socketID;
 
 // separating client id
-      char *sep;
-      sep = strtok(clientMsg, ":");
+	char *sep;
+	sep = strtok(clientMsg, ":");     // sep holds in the first token now check if the first token is contain number or not
+	
+      	bool isDigit = 1;
+      	for(int i = 0; i < strlen(sep); i++)
+      	{
+        	if(isdigit(sep[i]) == 0)
+        	{
+          		isDigit = 0;
+        	}
+      	}
 
-      // sep holds in the first token now check if the first token is contain number or not
-      int isDigit = 1;
-      for(int i = 0; i < strlen(sep); i++){
-        if(isdigit(sep[i]) == 0){
-          isDigit = 0;
-        }
-      }
+// If token is digit
+      
+      	if(isDigit)
+      	{
+      		uint8_t id;
+        	id = atoi(sep) - 1;  //convert string token to int
+        	sep = strtok(NULL, ":");   // getting the message token part
+        
+        	char *emoji[] = {"LAUGH","HEART","SMILE"};
+		char *unicode[] = {LAUGH,HEART,SMILE};
+	
+		for(int i=0;i<3;i++)
+		{
+			sep = replaceWord(sep,emoji[i],unicode[i]);  //for replacing the message for emoji unicode
+		}
+	
+//if client is active then send msg otherwise send reciver client is not active to sender client
+        	if(strcmp(Client[id].status ,"ACTIVE") == 0 && sep)
+        	{ 
+           		sprintf(output, "\033[1;32m [msg] From Client %d : %s\033[1:0m", index+1, sep);  //sending the message to that client
 
-      // Convert client id from char to int
-      int id;
-
-      if(isDigit){
-        id = atoi(sep) - 1;
-
-        // getting the message token part
-        sep = strtok(NULL, ":");
-
-        //if client is active then send msg otherwise send reciver client is not active to sender client
-        if(strcmp(Client[id].status ,"ACTIVE") == 0 && sep){
-
-          //sending the message to that client 
-          //int l = 0;
-           sprintf(output, "[msg] From Client %d : %s", index+1, sep);
-
-    
-          if(send(Client[id].socketID, output, strlen(output), 0) < 0){
-            printf("[--] Can't send message...\n");
-          }
-
-          // server response to the client for sending message successfully
+          		if(send(Client[id].socketID, output, strlen(output), 0) < 0)
+          		{
+            			perror("\033[1;32m[--] Can't send message...\033[1;0m\n");
+          		}
   
-          memset(output,'\0',sizeof(output));
-          sprintf(output, "[res] Message sent to client %d...\n", id+1);
+          		memset(output,'\0',sizeof(output));
+          		sprintf(output, "\033[1;34m[res] Message sent to client %d...\033[1;0m\n", id+1);  // server response to the client
+			printf(" ..... Successfully send the message to client %d from client %d .....\n", id+1, index+1);
+        	}
+ 
+        	else
+        	{
+          		sprintf(output, "\033[1;34m[res] Cannot send the message since client %d is DE-ACTIVE...\033[1;0m\n", id+1);
+        	}
+      	}
+      	
+// If token is not digit      	
+      	else
+      	{
+        	strcpy(output, "\033[1;34m[res] WRONG CLIENT ID...\033[1;0m\n");
+      	}
 
-          printf(" ..... Successfully send the message to client %d from client %d .....\n", id+1, index+1);
-        }
-        else{
-          sprintf(output, "[res] Cannot send the message since client %d is DE-ACTIVE...\n", id+1);
-        }
-      }
-      else{
-        strcpy(output, "[res] WRONG CLIENT ID...\n");
-      }
-
-      if(send(clientSocket, output, strlen(output), 0) < 0){
-      printf("[--] Can't send message...\n");
-    }
-    }
+      	if(send(clientSocket, output, strlen(output), 0) < 0)
+      	{
+      		perror("\033[1;31m[--] Can't send message...\033[1;0m\n");
+    	}
+}
