bad45b5bc6582aa50e0d84288acc885e6a3a6556
Sairaj0603 <152605079+Sairaj0603@users.noreply.github.com>
2023-12-02 22:56:02 -0800

    Update server_msg.c

--- server_msg.c
+++ server_msg.c
@@ -1,7 +1,7 @@
 
 #include "header.h"
 
-extern uint8_t clientCount;
+//extern uint8_t clientCount;
 
 char output[MAX];
 extern struct client Client[10];
@@ -33,10 +33,10 @@ void message(void* ClientDetail,char* clientMsg)
         	id = atoi(sep) - 1;  //convert string token to int
         	sep = strtok(NULL, ":");   // getting the message token part
         
-        	char *emoji[] = {"LAUGH","HEART","SMILE"};
-		char *unicode[] = {LAUGH,HEART,SMILE};
+        	char *emoji[] = {"LAUGH","HEART","SMILE","ANGRY","OK"};
+		char *unicode[] = {LAUGH,HEART,SMILE,ANGRY,OK};
 	
-		for(int i=0;i<3;i++)
+		for(int i=0;i<5;i++)
 		{
 			sep = replaceWord(sep,emoji[i],unicode[i]);  //for replacing the message for emoji unicode
 		}
@@ -44,28 +44,28 @@ void message(void* ClientDetail,char* clientMsg)
 //if client is active then send msg otherwise send reciver client is not active to sender client
         	if(strcmp(Client[id].status ,"ACTIVE") == 0 && sep)
         	{ 
-           		sprintf(output, "\033[1;32m [msg] From Client %d : %s\033[1:0m", index+1, sep);  //sending the message to that client
+           		sprintf(output, "\n\033[1;32m[msg] From Client %d : %s\033[1;0m\n", index+1, sep);  //sending the message to that client
 
           		if(send(Client[id].socketID, output, strlen(output), 0) < 0)
           		{
-            			perror("\033[1;32m[--] Can't send message...\033[1;0m\n");
+            			perror("\033[1;31m[--] Can't send message...\033[1;0m\n");
           		}
   
           		memset(output,'\0',sizeof(output));
-          		sprintf(output, "\033[1;34m[res] Message sent to client %d...\033[1;0m\n", id+1);  // server response to the client
-			printf(" ..... Successfully send the message to client %d from client %d .....\n", id+1, index+1);
+          		sprintf(output, "\n\033[1;34m[res] Message sent to client %d...\033[1;0m\n", id+1);  // server response to the client
+			printf("\033[1;32m ..... Successfully send the message to client %d from client %d .....\033[1;0m\n", id+1, index+1);
         	}
  
         	else
         	{
-          		sprintf(output, "\033[1;34m[res] Cannot send the message since client %d is DE-ACTIVE...\033[1;0m\n", id+1);
+          		sprintf(output, "\n\033[1;31m[res] Cannot send the message since client %d is DE-ACTIVE...\033[1;0m\n", id+1);
         	}
       	}
       	
 // If token is not digit      	
       	else
       	{
-        	strcpy(output, "\033[1;34m[res] WRONG CLIENT ID...\033[1;0m\n");
+        	strcpy(output, "\n\033[1;31m[res] WRONG CLIENT ID...\033[1;0m\n");
       	}
 
       	if(send(clientSocket, output, strlen(output), 0) < 0)
